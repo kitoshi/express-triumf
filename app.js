4d82111293a40d454c33b0069c1afb426fe1ca33
kitoshi <kitoshi.charlton@gmail.com>
2022-10-20 14:14:36 -0700

    5 second timer interval to pull olis data

--- app.js
+++ app.js
@@ -10,20 +10,42 @@ const port = 8081;
 
 app.use(cors());
 
-app.get(`/`, async function (req, res) {
+let laserDirectionVariables;
+let olisVariables;
+laserDirectionVariables = await getLaserDirectionVariables();
+olisVariables = await getOlisVariables();
+
+async function getLaserDirectionVariables() {
   const url =
     'https://beta.hla.triumf.ca/jaya-isac/IOS:XCB1AW:RDVOL+IOS:XCB1AE:RDVOL+IOS:PSWXCB1A:STATON';
   const options = {
     method: 'GET'
   };
+  let response = await fetch(url, options);
+  return await response.text();
+}
+
+async function getOlisVariables() {
+  const url =
+    'https://beta.hla.triumf.ca/jaya-isac/IOS:BEAMTYPE+IOS:BIAS:VOL+IOS:HALLMB:RDFIELD+IOS:IG1:RDVAC+IOS:IG2:RDVAC+IOS:MB:MASSOVERQ2+ISAC:IOSBEAMPATH+MCIS:BIAS0:VOL+MCIS:EL1:VOL+MCIS:IG1:RDVAC+MCIS:RFS1:AMPL+MCIS:RFS1:FREQ+MCIS:RFS2:AMPL+MCIS:RFS2:FREQ';
+  const options = {
+    method: 'GET'
+  };
+  let response = await fetch(url, options);
+  return await response.text();
+}
+
+setInterval(async () => {
+  laserDirectionVariables = await getLaserDirectionVariables();
+  olisVariables = await getOlisVariables();
+}, 5000);
+
+app.get(`/`, async function (req, res) {
   try {
-    let response = await fetch(url, options);
-    const text = await response.text();
-    const xmlObject = JSON.parse(convert.xml2json(text));
-    // res.status(200).json(xmlObject);
+    console.log(laserDirectionVariables);
+    const xmlObject = JSON.parse(convert.xml2json(laserDirectionVariables));
     let data = parse_xml(xmlObject);
     res.status(200).json(data[0]);
-    // console.log(JSON.parse(convert.xml2json(text)));
   } catch (err) {
     console.log(err);
     res.status(500).json({ msg: `Internal Server Error.` });
@@ -31,19 +53,10 @@ app.get(`/`, async function (req, res) {
 });
 
 app.get(`/OLIS`, async function (req, res) {
-  const url =
-    'https://beta.hla.triumf.ca/jaya-isac/IOS:BEAMTYPE+IOS:BIAS:VOL+IOS:HALLMB:RDFIELD+IOS:IG1:RDVAC+IOS:IG2:RDVAC+IOS:MB:MASSOVERQ2+ISAC:IOSBEAMPATH+MCIS:BIAS0:VOL+MCIS:EL1:VOL+MCIS:IG1:RDVAC+MCIS:RFS1:AMPL+MCIS:RFS1:FREQ+MCIS:RFS2:AMPL+MCIS:RFS2:FREQ';
-  const options = {
-    method: 'GET'
-  };
   try {
-    let response = await fetch(url, options);
-    const text = await response.text();
-    const xmlObject = JSON.parse(convert.xml2json(text));
-    // res.status(200).json(xmlObject);
+    const xmlObject = JSON.parse(convert.xml2json(olisVariables));
     let data = parse_xml(xmlObject);
     res.status(200).json(data[0]);
-    // console.log(JSON.parse(convert.xml2json(text)));
   } catch (err) {
     console.log(err);
     res.status(500).json({ msg: `Internal Server Error.` });
